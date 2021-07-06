10314ebeddc0aa44db6e7a57beecfac1dc51ec11
Juan Garcia Giraldo <juan.garciagiraldo@student.manchester.ac.uk>
2021-07-06 16:40:33 +0100

    Updating FactBankIRServer to have the latests endpoints used by the UI

--- FactBankIRServer.py
+++ FactBankIRServer.py
@@ -5,9 +5,10 @@ import json
 import math
 import numpy as np
 import heapq
+import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-#Mean Pooling - Take attention mask into account for correct averaging
+# Mean Pooling - Take attention mask into account for correct averaging
 def mean_pooling(model_output, attention_mask):
     token_embeddings = model_output[0] #First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
@@ -15,25 +16,19 @@ def mean_pooling(model_output, attention_mask):
     sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
     return sum_embeddings / sum_mask
 
-#Retrieve fact based on BioBERT
+# Retrieve fact based on BioBERT
 class RetrieveFact:
     
-    def __init__(self, fp, sen_emb_fp):#'output/facts_lst.jsonl'
-        #Load AutoModel from huggingface model repository
+    def __init__(self, fp, sen_emb_fp):
+        # Load AutoModel from huggingface model repository
         self.tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-v1.1")
         self.bert = AutoModel.from_pretrained("dmis-lab/biobert-v1.1")
-        self.fp = fp
 
         print('BERT model loaded')
         
-        self.facts_lst=[]
-        with open(fp,'r') as f:
-            line=f.readline()
-            while line:
-                self.facts_lst.append(json.loads(line))
-                line=f.readline()
+        self.breastcancer_facts_df=pd.read_csv(fp)
 
-        #self.encoded_inputs=torch.load(enc_inp_fp)
+        # self.encoded_inputs=torch.load(enc_inp_fp)
     
         with open(sen_emb_fp,'rb') as f:
             self.sentence_embeddings=np.load(f)
@@ -49,73 +44,77 @@ class RetrieveFact:
         cos_sim=cosine_similarity(query_embedding,self.sentence_embeddings)[0].tolist()
 
         return [
-            {'cosine_similarity':cos_sim[i],'sentence':self.facts_lst[i][1],
-             'paragraph':self.facts_lst[i][2],'pmcid':self.facts_lst[i][3],
-             'unique_id':self.facts_lst[i][0],'type':self.facts_lst[i][4],
-             'title':self.facts_lst[i][5],'author_citation':self.facts_lst[i][6]
+            {
+                'cosine_similarity':cos_sim[i],
+                'sentence':self.breastcancer_facts_df.sentence[i],
+                'title':self.breastcancer_facts_df.title[i],
+                'citation':self.breastcancer_facts_df.citation[i],
+                'unique_id':self.breastcancer_facts_df.unique_id[i]
             }
-            for i in heapq.nlargest(20,range(len(self.facts_lst)),key=lambda i:cos_sim[i])
+            for i in heapq.nlargest(20,range(len(self.breastcancer_facts_df)),key=lambda i:cos_sim[i])
         ]
 
-#retrieve_fact=RetrieveFact(fp='output/facts_lst.jsonl',sen_emb_fp='output/sentence_embeddings.npy')
-retrieve_fact=RetrieveFact(fp='output/sampled_40_facts_lst.jsonl',sen_emb_fp='output/sampled_40_sentence_embeddings.npy')
+retrieve_fact=RetrieveFact(fp='output/BreastCancerFacts.csv',sen_emb_fp='output/breastcancerfacts_embeddings.npy')
 
+# HTTP SERVER ------------------------------------------------------------------------------------------------------------------------------------------
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
-from urllib.parse import urlparse,parse_qs
-import cgi
+from urllib.parse import urlparse, parse_qs
 import json
+import logging
+import uuid
 
-class S(BaseHTTPRequestHandler):
-    def _set_html_response(self):
+class MyHandler(BaseHTTPRequestHandler):
+
+    def _send_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        self.send_header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
-
-    def _set_json_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+    
+    def _send_error(self):
+        self.send_response(400)
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:3000')
+        self.send_header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
 
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        query=urlparse(self.path)
-        #print(query)
-        if query.path=='/home':
-            self._set_html_response()
-            with open('SearchHome.html','r') as f:
-                line=f.readline()
-                while line:
-                    self.wfile.write(line.encode('utf-8'))
-                    line=f.readline()
+        logging.info('GET request,\nPath: %s\nHeaders:\n%s\n', str(self.path), str(self.headers))
+        url = urlparse(self.path)
+        fields = parse_qs(url.query)
+        if url.path == '/search' and 'query' in fields:
+            self._send_headers()
+            self.wfile.write(json.dumps(retrieve_fact.retrieve(fields['query'][0])).encode('utf-8'))
         else:
-            self.error_page()
+            self._send_error()
+            self.wfile.write('Invalid URL'.encode('utf-8'))
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
-        query=urlparse(self.path)
-        #print(query)
-        if query.path=='/search':
-            fields = parse_qs(post_data.decode('utf-8'))
-            #print(fields)
-            if 'search' in fields:
-                self._set_json_response()
-                self.wfile.write(json.dumps(retrieve_fact.retrieve(fields['search'][0])).encode('utf-8'))
-            else:
-                self.error_page()
+        content_length = int(self.headers['Content-Length'])
+        post_data = json.loads(self.rfile.read(content_length).decode('utf-8'))
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",str(self.path), str(self.headers), post_data)
+
+        url = urlparse(self.path)
+        if url.path == '/tree' and 'treeData' in post_data:
+            treeId = str(uuid.uuid4())
+            newFactTree = {"treeId": treeId, "treeData": post_data['treeData']}
+            with open('tree/%s.json' % treeId, 'w') as file:
+                json.dump(newFactTree, file)
+
+            self._send_headers()
+            self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         else:
-            self.error_page()
+            self._send_error()
+            self.wfile.write('Invalid URL'.encode('utf-8'))
 
-    def error_page(self):
-        self._set_html_response()
-        #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-        self.wfile.write('error request'.encode('utf-8'))
+    def do_OPTIONS(self):
+        self._send_headers()
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
+def run(server_class = HTTPServer, handler_class = MyHandler, port = 8080):
+    logging.basicConfig(level = logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     logging.info('Starting httpd...\n')
